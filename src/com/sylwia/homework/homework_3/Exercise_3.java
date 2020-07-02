8dd1cdb07c99ba2ef123aba587b85dfb785dac99
piotrowska-92 <piotrowska.sylwia01@gmail.com>
2020-07-02 16:46:41 +0200

    Fix exercise 3

--- src/com/sylwia/homework/homework_3/Exercise_3.java
+++ src/com/sylwia/homework/homework_3/Exercise_3.java
@@ -7,8 +7,19 @@ public class Exercise_3 {
     public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        System.out.println("Wpisz teskt");
-//        = sc.next()
+        String longestLine = "";
+        String line;
+        int i =0;
 
+        while (i++ < 10) {
+            System.out.println("Wpisz tekst");
+            line = sc.nextLine();
+
+            if (line.length() > longestLine.length()) {
+                longestLine = line;
+            }
+        }
+
+        System.out.println("Najdluzsza linijka: " + longestLine);
     }
 }
