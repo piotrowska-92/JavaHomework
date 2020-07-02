6a20b1d619ce7a5fc6fb4b30f5d39830d3d69461
piotrowska-92 <piotrowska.sylwia01@gmail.com>
2020-07-02 17:39:19 +0200

    Exercise 5

--- src/com/sylwia/homework/homework_3/Exercise_5.java
+++ src/com/sylwia/homework/homework_3/Exercise_5.java
@@ -1,4 +1,28 @@
 package com.sylwia.homework.homework_3;
 
+import java.util.Scanner;
+
 public class Exercise_5 {
+
+    // 1011 = 1*2^0 + 1*2^1 + 1*2^3
+    public static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        System.out.println("Podaj liczbe");
+        String bin = sc.next();
+
+        System.out.println("Wartosc dziesietna: " + binToInt(bin));
+    }
+
+    public static int binToInt(String binaryValue) {
+        int sum = 0;
+
+        for (int i = 0; i < binaryValue.length(); i++) {
+            if (binaryValue.charAt(i) == '1') {
+                sum += Math.pow(2, binaryValue.length() - 1 - i);
+            }
+        }
+
+        return sum;
+    }
 }
