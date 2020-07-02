dbcf2d6639a5895916dbd175413c2dc7deb8488a
piotrowska-92 <piotrowska.sylwia01@gmail.com>
2020-07-02 17:01:21 +0200

    Fix exercise 4

--- src/com/sylwia/homework/homework_3/Exercise_4.java
+++ src/com/sylwia/homework/homework_3/Exercise_4.java
@@ -6,6 +6,26 @@ public class Exercise_4 {
     public static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
+        String binWithMostOnes = "";
+        int ones = 0;
+
+        for ( int i = 0; i <5; i++){
+            System.out.println("Podaj liczbe");
+            String bin = sc.next();
+
+            int sum = 0;
+            for (int k = 0; k < bin.length(); k++){
+                sum += Character.getNumericValue(bin.charAt(k));
+            }
+
+            if (sum > ones) {
+                binWithMostOnes = bin;
+                ones = sum;
+            }
+        }
+
+        System.out.println("Najwiecej 1 ma: " + binWithMostOnes);
+        System.out.println("Liczba 1: " + ones);
 
     }
 
